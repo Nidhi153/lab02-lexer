ab2bd1df02b9056e193cd3077c2a25fd00f7724c
Nidhi153 <npshahaa@gmail.com>
2026-03-23 21:43:50 +0800

    feat: more tests

--- test/scala/amyc/test/LexerTests.scala
+++ test/scala/amyc/test/LexerTests.scala
@@ -12,4 +12,18 @@ class LexerTests extends TestSuite {
 
   @Test def testKeywords = shouldOutput("Keywords")
 
+  // Passing tests
+  @Test def testOperators = shouldOutput("Operators")
+  @Test def testIdentifiers = shouldOutput("Identifiers")
+  @Test def testLiterals = shouldOutput("Literals")
+  @Test def testDelimiters = shouldOutput("Delimiters")
+  @Test def testComments = shouldOutput("Comments")
+  @Test def testWhitespace = shouldOutput("Whitespace")
+
+  // Failing tests
+  @Test def testUnclosedComment = shouldFail("UnclosedComment")
+  @Test def testUnclosedString = shouldFail("UnclosedString")
+  @Test def testInvalidToken = shouldFail("InvalidToken")
+  @Test def testIntegerOverflow = shouldFail("IntegerOverflow")
+
 }
